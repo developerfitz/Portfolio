cf2601bb4b5d50e8024e97a6b57846f53a93c85d
developerfitz <fitz3364@gmail.com>
2020-08-15 03:11:26 -0400

    🔥 chore(): removing pages until production ready

--- src/pages/about.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-
-const About = () => (
-  <h1>I'm an Engineer!</h1>
-)
-
-export default About
--- src/pages/skills.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import TestTemplate from '../templates/test'
-
-
-export default function Skills() {
-  return (
-    <TestTemplate />  
-  )
-}
